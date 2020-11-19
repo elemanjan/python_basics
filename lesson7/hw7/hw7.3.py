534cae08d91f49e739b181ceb14e47d8eb2a6a5b
elemanjan <namele93@gmail.com>
2020-11-19 10:32:14 +0600

    7.3

--- lesson7/hw7/hw7.3.py
+++ lesson7/hw7/hw7.3.py
@@ -1,1 +1,10 @@
+def multStr():
+    num = int(input("Enter number to multiply string: "))
+    st = input("Enter string: ")
+    s = ""
+    for i in range(0, num, 1):
+        s += st
+    print(s)
 
+
+multStr()
