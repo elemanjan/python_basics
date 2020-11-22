1bac624822c302f526d25abfadc55e2514984a96
elemanjan <namele93@gmail.com>
2020-11-23 03:40:52 +0600

    hw9

---
+++ lesson9/hw9/abc/len_word.py
@@ -0,0 +1,6 @@
+from ..defg import *
+import sys
+sys.path.append('../')
+
+c = countWord(input())
+print(c)
---
+++ lesson9/hw9/defg/my_lib.py
@@ -0,0 +1,4 @@
+def countWord(word):
+    word = word.split()
+    length = len(word)
+    return length
