fcd408effd7b77c4c462b708a375d8b471a0e9df
elemanjan <namele93@gmail.com>
2020-12-28 17:11:07 +0600

    lesson 23 24

---
+++ lesson23/script.js
@@ -0,0 +1,5 @@
+sName = prompt('Введите фамилию');
+fName = prompt('Введите имя');
+
+console.log('Моя фамилия: ' + sName);
+console.log('Мое имя: ' + fName);
---
+++ lesson24/script.js
@@ -0,0 +1,9 @@
+a = Number(prompt('Введите число a'));
+b = Number(prompt('Введите число b'));
+document.write('Сумма: ', a+b, "</br>");
+document.write('Умножение: ', a*b, "</br>");
+document.write('Деление: ', a/b, "</br>");
+document.write('Вычитание: ', a-b, "</br>");
+document.write('Возведение в степень: ', a**b, "</br>");
+document.write('a: ', Boolean(a), "</br>");
+
